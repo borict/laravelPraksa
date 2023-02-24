121af056705210b6c8dfe63ddd728efc5fe71207
borict <tamara.boric8@gmail.com>
2023-02-24 12:01:51 +0100

    Created middleware

---
+++ app/Http/Middleware/CheckAge.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $age = 20;
+        if ($age < 18) {
+            return response()->json(['message' => 'you are too young']);
+        }
+        return $next($request);
+    }
+}
